8923d6cf10f6891d562c9449bec73c2375f93827
Martin Škorupa <majoskorec@gmail.com>
2026-03-22 14:49:04 +0100

    easy admin init

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,20 +4,33 @@ declare(strict_types=1);
 
 namespace App\Controller\Admin;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
 
-final class DashboardController extends AbstractController
+#[AdminDashboard(
+    routePath: '/admin',
+    routeName: DashboardController::ROUTE_NAME,
+)]
+final class DashboardController extends AbstractDashboardController
 {
     public const string ROUTE_NAME = 'admin_dashboard';
 
-    #[Route(
-        path: '/admin',
-        name: self::ROUTE_NAME,
-    )]
-    public function __invoke(): Response
+    public function index(): Response
     {
         return $this->render('admin/dashboard/index.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Shooting Competition');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'internal:home');
     }
 }
