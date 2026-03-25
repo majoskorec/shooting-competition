2040cd5e1832f9149b866be27cec7229e4355a6c
Martin Škorupa <majoskorec@gmail.com>
2026-03-25 06:08:01 +0100

    composer update

--- importmap.php
+++ importmap.php
@@ -20,17 +20,17 @@ return [
         'path' => './assets/admin.js',
         'entrypoint' => true,
     ],
-    '@hotwired/stimulus' => [
-        'version' => '3.2.2',
-    ],
     '@symfony/stimulus-bundle' => [
         'path' => './vendor/symfony/stimulus-bundle/assets/dist/loader.js',
     ],
-    '@hotwired/turbo' => [
-        'version' => '7.3.0',
-    ],
     '@symfony/ux-live-component' => [
         'path' => './vendor/symfony/ux-live-component/assets/dist/live_controller.js',
+    ],
+    '@hotwired/stimulus' => [
+        'version' => '3.2.2',
+    ],
+    '@hotwired/turbo' => [
+        'version' => '8.0.23',
     ],
     'bootstrap' => [
         'version' => '5.3.8',
